0a2d723d286f3c7501074c88a06c9cf4592a1220
nvhoa2005 <hoa049170@gmail.com>
2025-09-10 12:47:34 +0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nguyen Van Hoa",
+    author_email="hoa049170@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
