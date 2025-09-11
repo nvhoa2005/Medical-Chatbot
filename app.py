e5571edbfb2ab05aa76f5246f67e69b9564122d8
nvhoa2005 <hoa049170@gmail.com>
2025-09-11 20:36:42 +0700

    medical chatbot modular coding

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, jsonify, request
+from src.helpers import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+
+embeddings = download_hugging_face_embeddings()
+
+index_name = "medicalchatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+
+
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chatModel = ChatOpenAI(model="gpt-4o")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+response = rag_chain.invoke({"input": "what is the Treatment of Acne?"})
+print(response["answer"])
+
+response = rag_chain.invoke({"input": "what is Acne?"})
+print(response["answer"])
